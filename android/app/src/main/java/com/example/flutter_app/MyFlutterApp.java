f83418de7b49a22ffe159321d5886ff22af5eabb
liyujiang <liyujiang_tk@yeah.net>
2019-08-27 18:24:22 +0800

    防止APP字体随系字体大小改变造成的布局错位问题

--- android/app/src/main/java/com/example/flutter_app/MyFlutterApp.java
+++ android/app/src/main/java/com/example/flutter_app/MyFlutterApp.java
@@ -1,5 +1,8 @@
 package com.example.flutter_app;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
 import io.flutter.app.FlutterApplication;
 
 /**
@@ -13,6 +16,16 @@ public class MyFlutterApp extends FlutterApplication {
     public void onCreate() {
         super.onCreate();
         android.util.Log.w(null, "Android native is finished");
+    }
+
+    // 解决APP字体随系字体大小改变造成的布局错位问题
+    @Override
+    public Resources getResources() {
+        Resources resources = super.getResources();
+        Configuration configuration = new Configuration();
+        configuration.setToDefaults();
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+        return resources;
     }
 
 }
